7054af0de484723ffbbef10d589aab5cd50b14ad
ocawthorne <ocawthorne@gmail.com>
2021-01-10 18:04:19 +0000

    task-lister-lite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTask = document.createElement("li");
+  newTask.innerText = document.getElementById("new-task-description").value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
